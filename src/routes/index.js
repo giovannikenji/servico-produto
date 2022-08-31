646955c2d2649f2f28c4bcaac261b5e9e63403c4
Giovanni Kenji Mendes <giovannikenjimendes1@gmail.com>
2022-08-31 20:54:27 -0300

    adicionar uma propriedade no json da index.js #6

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,10 @@ const router = new express.Router();
 //primeiro ponto de acesso para a nossa aplicação
 //endpoint
 router.get('/', (req, res, next) => {
-    res.status(200).send("Alterar resposta!")
+    res.status(200).send({
+        "nome" : "Giovanni Kenji Mendes",
+        "idade" : 20
+    })
 });
 
 module.exports = router; //expor a rota
